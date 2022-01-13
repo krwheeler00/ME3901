d339a06ee621a4c5787f311ff82f1d0b38665e42
krwheeler00 <59895344+krwheeler00@users.noreply.github.com>
2022-01-13 15:22:54 -0500

    blink 3 LEDs

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,35 +1,39 @@
 //constants
-int led_pin1 1
-int led_pin2 2
-int led_pin3 3
-int buttonPin 4
-bool buttonState = false;
+#define led_pin1 10
+#define led_pin2 11
+#define led_pin3 12
+#define buttonPin 4
+#define buttonState = false;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led_pin1, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(buttonPin),ISR, CHANGE); 
+  pinMode(led_pin2, OUTPUT);
+  pinMode(led_pin3, OUTPUT);
+  
+ // attachInterrupt(digitalPinToInterrupt(buttonPin),ISR, CHANGE); 
 }
 
 void loop() {
   // part A of the lab: blink an LED on and off repeatedly, used also for part E with the potentiometer 
-  digitalWrite(led_pin1, HIGH);
-  delay(1000);
-  digitalWrite(led_pin1, LOW);
-  delay(1000);
+//  digitalWrite(led_pin1, HIGH);
+//  delay(1000);
+//  digitalWrite(led_pin1, LOW);
+//  delay(1000);
 
   //part B of the lab: blink multiple LEDS sequentially
-//  digitalWrite(led_pin1, HIGH);
-//  digitalWrite(led_pin2, LOW);
-//  digitalWrite(led_pin3, LOW);
-//  delay(1000);
-//  digitalWrite(led_pin1, LOW);
-//  digitalWrite(led_pin2, HIGH);
-//  digitalWrite(led_pin3, LOW);
-//  delay(1000);
-//  digitalWrite(led_pin1, LOW);
-//  digitalWrite(led_pin2, LOW);
-//  digitalWrite(led_pin3, HIGH);
+  digitalWrite(led_pin1, HIGH);
+  digitalWrite(led_pin2, LOW);
+  digitalWrite(led_pin3, LOW);
+  delay(1000);
+  digitalWrite(led_pin1, LOW);
+  digitalWrite(led_pin2, HIGH);
+  digitalWrite(led_pin3, LOW);
+  delay(1000);
+  digitalWrite(led_pin1, LOW);
+  digitalWrite(led_pin2, LOW);
+  digitalWrite(led_pin3, HIGH);
+  delay(1000);
 
   //part C of the lab - fade sequence with PWM and LED
 //   for (int i = 0; i <= 255; i++) {
@@ -49,6 +53,6 @@ void loop() {
 }
 
 //interrupt service routine to execute on button press 
-void ISR(){
-  buttonState = !buttonState;
-}
+//void ISR(){
+//  buttonState = !buttonState;
+//}
