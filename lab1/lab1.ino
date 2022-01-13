738306f953e8006be3ed1fe7f86ce939eea87416
krwheeler00 <59895344+krwheeler00@users.noreply.github.com>
2022-01-13 14:55:49 -0500

    idk man

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -2,6 +2,7 @@
 int led_pin1 1
 int led_pin2 2
 int led_pin3 3
+int buttonPin 4
 bool buttonState = false;
 
 void setup() {
@@ -18,33 +19,33 @@ void loop() {
   delay(1000);
 
   //part B of the lab: blink multiple LEDS sequentially
-  digitalWrite(led_pin1, HIGH);
-  digitalWrite(led_pin2, LOW);
-  digitalWrite(led_pin3, LOW);
-  delay(1000);
-  digitalWrite(led_pin1, LOW);
-  digitalWrite(led_pin2, HIGH);
-  digitalWrite(led_pin3, LOW);
-  delay(1000);
-  digitalWrite(led_pin1, LOW);
-  digitalWrite(led_pin2, LOW);
-  digitalWrite(led_pin3, HIGH);
+//  digitalWrite(led_pin1, HIGH);
+//  digitalWrite(led_pin2, LOW);
+//  digitalWrite(led_pin3, LOW);
+//  delay(1000);
+//  digitalWrite(led_pin1, LOW);
+//  digitalWrite(led_pin2, HIGH);
+//  digitalWrite(led_pin3, LOW);
+//  delay(1000);
+//  digitalWrite(led_pin1, LOW);
+//  digitalWrite(led_pin2, LOW);
+//  digitalWrite(led_pin3, HIGH);
 
   //part C of the lab - fade sequence with PWM and LED
-   for (int i = 0; i <= 255; i++) {
-    analogWrite(led_pin1, i);
-    delay(10);
-  }
+//   for (int i = 0; i <= 255; i++) {
+//    analogWrite(led_pin1, i);
+//    delay(10);
+//  }
 
   //part D of the lab- complete part B with button
-  if (buttonState){
-     digitalWrite(led_pin1, HIGH);
-     delay(1000);
-     digitalWrite(led_pin1, LOW);
-     delay(1000);
-  }else{
-    digitalWrite(led_pin1, LOW);
-  }
+//  if (buttonState){
+//     digitalWrite(led_pin1, HIGH);
+//     delay(1000);
+//     digitalWrite(led_pin1, LOW);
+//     delay(1000);
+//  }else{
+//    digitalWrite(led_pin1, LOW);
+//  }
 }
 
 //interrupt service routine to execute on button press 
